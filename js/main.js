9a3d3f0e1e6967da5c3547f697a75fce85b6308c
Glenn Ko <glidias@hotmail.com>
2012-10-12 13:00:18 +0800

    improved examples a bit

--- js/main.js
+++ js/main.js
@@ -5,5 +5,38 @@
  
 (function() {
 	
+	// DEFAULT TRANSITION IN/OUT METHOD DECLRATIONS
+	function defaultTransitionIn(callback, content) {
+		content.css("opacity", 0);
+		content.animate({"opacity":1}, 780, null,callback );
+		
+	}
+	function defaultTransitionOut(callback, content) {
+		content.animate({"opacity":0}, 780, null,callback );
+	
+	}	
+	Gaiajax.api.setDefaultTransitionIn(defaultTransitionIn);
+	Gaiajax.api.setDefaultTransitionOut(defaultTransitionOut);
+	
+	
+	
+	//GLOBAL NAVIGATION
+	var _myNavLinks = $("#nav");
+	var _myNavLinksA = $("#nav a"); // the individual links within nav holder
+	var _isHomeOut;
+
+	function onAfterGoto() {	
+		var src;	
+		var paths = Gaiajax.api.getValidBranches(Gaiajax.api.getTargetPage().path);
+
+		var i = paths.length;
+		_myNavLinksA.removeClass("selected");
+		while(--i > -1) {
+			src = Gaiajax.api.getPage(paths[i]).src;
+			_myNavLinks.find('a[href="'+src+'"]').addClass("selected");
+		}
+	}
+	Gaiajax.api.onAfterGoto.add(onAfterGoto);
+	onAfterGoto();
 	
 })();
